0f2c2d4e33af8e871ef79cea57eb8d06d7c06098
Gavin Gong <Gavin Gong>
2017-03-01 21:50:48 +0800

    CBF

---
+++ CountingBF/CountingBF.h
@@ -0,0 +1,21 @@
+#ifndef _COUNTINGBF_H
+#define _COUNTINGBF_H
+
+#include "common.h"
+
+class CountingBF
+{
+public:
+	CountingBF(int k, int w, int b);
+	void Insert(cuc *str, uint num = 1);
+	uint Query(cuc *str);
+	
+private:
+	int k;
+	int width;
+	int bits;
+	HashFunction *hash;
+	uint *filter;
+};
+
+#endif
